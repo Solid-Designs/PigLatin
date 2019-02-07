85952bf8bfef98fca8c0f2c408c55c941ae277a8
thesolidmoose <solid.moose15@gmail.com>
2019-02-07 11:22:36 -0600

    Finished

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,19 @@ public class Main{
     // Create pig latin
     public static void main(String[] args){
         // Create if for consonant and vowel word rules
-        String word = "Pig";
+        String word = "Egg";
         char[] wordArray = word.toCharArray();
-        char firstLetter;
+        char firstLetter = wordArray[0];
 
-        if(wordArray[0] == 'A' || wordArray[0] == 'E' || wordArray[0] == 'I'
+        if(firstLetter == 'A' || wordArray[0] == 'E' || wordArray[0] == 'I'
                 || wordArray[0] == 'O' || wordArray[0] == 'U'){
             System.out.println("This starts with a vowel");
+            System.out.println(word + "ay");
         }else{
             System.out.println("This starts with a consonant");
-            firstLetter = wordArray[0];
+            // Make the first character null/empty
             wordArray[0] = '\0';
+
             for(char i : wordArray){
                 System.out.print(i);
             }
